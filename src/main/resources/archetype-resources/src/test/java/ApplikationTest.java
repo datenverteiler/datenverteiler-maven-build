48369dc64eb4297d69cd5e3418c538c0f4822871
Falko Schumann <falko.schumann@muspellheim.de>
2016-01-19 23:00:19 +0100

    Archetype für eine Applikation angelegt
    Das Maven-Projekt für eine Applikation wird nach den Anforderungen an
    das Maven Central Repository erstellt.

---
+++ src/main/resources/archetype-resources/src/test/java/ApplikationTest.java
@@ -0,0 +1,38 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+/*
+ * Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.
+ * Copyright (C) 2016  Datenverteiler <https://github.com/datenverteiler>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ${package};
+
+import static org.junit.Assert.*;
+
+/**
+ * Unittest für Applikation.
+ *
+ * @author Datenverteiler, Falko Schumann
+ */
+public class ApplikationTest {
+
+    public void testApplikation() {
+        // TODO
+        assertTrue(true);
+    }
+
+}
---
+++ src/test/resources/projects/basic/reference/README.md
@@ -0,0 +1,4 @@
+Applikation
+===========
+
+Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.
